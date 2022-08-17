e81eef6c9a9d8d8023c6bb98665be7d5f7f3a696
Johnny T Lardera <111446413+pinoykendoka@users.noreply.github.com>
2022-08-18 03:12:38 +0800

    Update index_py.py
    added breakfast menu header and 3 items

--- index_py.py
+++ index_py.py
@@ -2,9 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New healthy Diner')
 
-streamlit.header('BreakFast Menu')
+streamlit.header('BreakFast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

52164ad2de8baabdbf5429f05e4c10a1fd121dd8
Johnny T Lardera <111446413+pinoykendoka@users.noreply.github.com>
2022-08-24 16:48:03 +0800

    Update index_py.py

--- index_py.py
+++ index_py.py
@@ -66,7 +66,7 @@ if streamlit.button('Add a Fruit to the List'):
 streamlit.header('View Our Fruit List - Add you Favorites!')
 if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list
+    my_data_rows = get_fruit_load_list()
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
 

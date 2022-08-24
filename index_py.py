d57916ac6fda969998f19e0fdd7cc5e34570b702
Johnny T Lardera <111446413+pinoykendoka@users.noreply.github.com>
2022-08-24 14:53:39 +0800

    Update index_py.py

--- index_py.py
+++ index_py.py
@@ -25,6 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('Fruityvice Fruit Avice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 

dda12390545b43fd25a33fd6cdc52cdc994ca932
Johnny T Lardera <111446413+pinoykendoka@users.noreply.github.com>
2022-08-24 15:57:21 +0800

    Update index_py.py

--- index_py.py
+++ index_py.py
@@ -44,7 +44,7 @@ except URLError as e:
 
 #streamlit.stop()
 
-strealit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
